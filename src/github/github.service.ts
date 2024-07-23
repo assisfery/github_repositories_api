19d9a3a9f32705e4d306ce0b79a2bf4d6990f783
Assis Ferreira <ferreira.assisruny@gmail.com>
2024-07-23 17:31:24 -0100

    feat: verify error in github requests

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -9,13 +9,27 @@ export class GithubService {
     async findUser(login: string) {
         const url = this.configService.get<string>('github_api_url');    
         const response = await fetch(url + '/users/' + login);
-        return response.json();
+        const body = await response.json();
+
+        if(!response.ok)
+        {
+            throw new HttpException(body.message ?? "Error in request", response.status);
+        }
+
+        return body;
     }
 
     async findRepositories(login: string) {
         const url = this.configService.get<string>('github_api_url');    
         const response = await fetch(url + '/users/' + login + '/repos');
-        return response.json();
+        const body = await response.json();
+
+        if(!response.ok)
+        {
+            throw new HttpException(body.message ?? "Error in request", response.status);
+        }
+
+        return body;
     }
 
 }
